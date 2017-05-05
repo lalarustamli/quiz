e73875e922334d5ef1a6fa2cd586be686d62e765
Lala Rustamli <rustamlilala@gmail.com>
2017-05-05 18:29:22 +0400

    Add files via upload

---
+++ Quiz.cs
@@ -0,0 +1,19 @@
+﻿namespace IntellectualQuiz
+{
+    public class Quiz
+    {
+        public int number;
+        public string question;
+        public string[] answers;
+        public string correct;
+        public Quiz(int number, string question, string answers, string correct)
+        {
+            this.number = number;
+            this.question = question;
+            this.answers = answers.Split(',');
+            this.correct = correct;
+
+        }
+    }
+
+}
